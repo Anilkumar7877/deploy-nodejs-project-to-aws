579ef7ebdecb905d99ac157215495c62796e3ec8
Anil Kumar <ad23b1004@iiitr.ac.in>
2026-02-16 03:32:59 +0530

    updated index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+﻿const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('hello word');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
